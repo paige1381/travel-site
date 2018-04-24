f9492f50f90e646fa70a3a042bf66bd62270c31f
Paige Boyer <paige1381@gmail.com>
2018-04-23 23:37:18 -0500

    added index and blog post pages

--- public/app.js
+++ public/app.js
@@ -14,12 +14,33 @@ app.controller('HomeController', ['$http', function($http) {
 
 }]);
 
+app.controller('PostsController', ['$http', function($http) {
+
+}]);
+
+app.controller('PostController', ['$http', function($http) {
+
+}]);
+
+
 app.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode({ enabled: true });
 
   $routeProvider.when('/', {
     templateUrl: 'home.html',
     controller: 'HomeController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/posts', {
+    templateUrl: 'posts.html',
+    controller: 'PostsController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/post', {
+    templateUrl: 'post.html',
+    controller: 'PostController',
     controllerAs: 'ctrl'
   });
 
